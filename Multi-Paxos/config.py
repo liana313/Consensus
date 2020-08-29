97e3a7bf036be7384b6fb557188690e759d699fe
liana313 <lianapatel515@gmail.com>
2020-08-29 14:14:15 -0400

    Multipaxos impl

---
+++ Multi-Paxos/config.py
@@ -0,0 +1,11 @@
+
+# (IP,UDP Port Number) # A will be leader proposer
+peers = dict( A=('127.0.0.1',10234),
+              B=('127.0.0.1',10235),
+              C=('127.0.0.1',10236) )
+
+# State files for crash recovery. Windows users will need to modify
+# these.
+state_files = dict( A='/tmp/A4.json',
+                    B='/tmp/B4.json',
+                    C='/tmp/C4.json' )
